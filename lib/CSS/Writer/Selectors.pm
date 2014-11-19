9c31e8356baf37b9ef72018c21a6d9db55f83c27
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-11-18 20:28:17 -0800

    implemented most common selector components

--- lib/CSS/Writer/Selectors.pm
+++ lib/CSS/Writer/Selectors.pm
@@ -6,12 +6,40 @@ class CSS::Writer::Selectors {
 
     use CSS::Grammar::AST :CSSSelector;
 
+    multi method write-selector( CSSSelector::SelectorList, List $ast ) {
+        join(', ',  $ast.map({ $.write( $_ ) }) );
+    }
+
+    multi method write-selector( CSSSelector::Selector, List $ast ) {
+        join(' ',  $ast.map({ $.write( $_ ) }) );
+    }
+
+    multi method write-selector( CSSSelector::SelectorComponent, List $ast ) {
+        [~] $ast.map({ $.write( $_ ) });
+    }
+
+    multi method write-selector( CSSSelector::Combinator, Str $ast ) {
+        $ast.lc;
+    }
+
+    multi method write-selector( CSSSelector::PseudoClass, Str $ast ) {
+        ':' ~ $.write( 'ident' => $ast );
+    }
+
     multi method write-selector( CSSSelector::PseudoFunction, Hash $ast ) {
         ':' ~ $.write( 'func' => $ast );
     }
 
     multi method write-selector( CSSSelector::Class, Str $ast ) {
         '.' ~ $.write( 'ident' => $ast );
+    }
+
+    multi method write-selector( CSSSelector::AttributeSelector, List $ast ) {
+        [~] '[', $ast.map({ $.write( $_ ) }), ']';
+    }
+
+    multi method write-selector( CSSSelector::AttributeOp, Str $ast ) {
+        $ast.lc;
     }
 
     multi method write-selector( CSSSelector::Id, Str $ast ) {
